94385f685b878f6104e385f71ac766eb233857e8
郝卓玉 <haozhuoyu@kaikeba.com>
2021-07-29 17:24:23 +0800

    feat: lisst show

--- src/controllers/system.ts
+++ src/controllers/system.ts
@@ -2,20 +2,29 @@
  * @Author: zz
  * @Date: 2021-06-29 20:30:01
  * @LastEditors: zz
- * @LastEditTime: 2021-07-12 15:11:24
+ * @LastEditTime: 2021-07-13 14:33:27
  */
 
 import { querySql } from '@/config/config.default';
 import { systemConfig } from '@/config';
-import { userNameSelect } from '@/services/system';
+import { userNameSelect, userNameList } from '@/services/system';
 import { Request, Response, NextFunction } from 'express';
 import { sendMes } from '@/utils/sendMes';
 const { CODE_SUCCESS } = systemConfig;
 import { UserInfo } from '@/controllers/model/userModel';
 
+type Data = {
+  data: UserInfo[];
+  total: number;
+};
+
 const userName = async (req: Request, res: Response, next: NextFunction) => {
-  const { user } = req.body;
-  const data: UserInfo[] = await querySql<UserInfo[]>(userNameSelect(user));
+  const list: UserInfo[] = await querySql<UserInfo[]>(userNameList());
+  const selecData: UserInfo[] = await querySql<UserInfo[]>(userNameSelect(req.body));
+  const data: Data = {
+    data: selecData,
+    total: list.length,
+  };
   res.json(sendMes(CODE_SUCCESS, 'success', data));
 };
 export { userName };
--- src/services/system.ts
+++ src/services/system.ts
@@ -2,7 +2,7 @@
  * @Author: zz
  * @Date: 2021-06-29 20:30:36
  * @LastEditors: zz
- * @LastEditTime: 2021-07-08 17:00:18
+ * @LastEditTime: 2021-07-13 14:35:15
  */
 
 /**
@@ -14,10 +14,12 @@
  */
 export const userNameSelect = ({ size = 10, page = 1, names = null }: any): any => {
   const start = (page - 1) * size;
-  const user = names ? `where username='${names}'`: `limit ${start},${size}`
-  return `select * from sys_user ${user}`;
+  if (!names) {
+    return `select * from sys_user limit ${start},${size}`;
+  }
+  return `select * from sys_user where username='${names}' limit ${start},${size}`;
 };
 
-// export const userListSelect = (size = 10, page: number):string => {
-//   return `select * from student limit(${page}-1)*${size},${size}`;
-// };
+export const userNameList = () => {
+  return 'select * from sys_user';
+};
