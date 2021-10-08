d211fec41160913b1ad43d2cab9c278f38ba985c
weizheng <weizheng@kaikeba.com>
2021-10-08 10:39:03 +0800

    fix: merge bug

--- src/schema/TypeDefs/Messages.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLObjectType, GraphQLString, GraphQLBoolean } from "graphql";
-
-export const MessageType = new GraphQLObjectType({
-  name: "Message",
-  fields: () => ({
-    success: { type: GraphQLBoolean },
-    message: { type: GraphQLString },
-  }),
-});
